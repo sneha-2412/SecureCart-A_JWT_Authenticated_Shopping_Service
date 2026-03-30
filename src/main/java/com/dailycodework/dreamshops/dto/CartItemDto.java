368c1ac4918a791d0bf901ae3585b44375d4aa97
Sneha <22h41f0052sneha@gmail.com>
2026-03-30 21:57:55 +0530

    Added Cart related Backend

---
+++ src/main/java/com/dailycodework/dreamshops/dto/CartItemDto.java
@@ -0,0 +1,10 @@
+package com.dailycodework.dreamshops.dto;
+
+import java.math.BigDecimal;
+
+public class CartItemDto {
+    private Long itemId;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private ProductDto product;
+}
